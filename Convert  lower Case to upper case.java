c709b20c00946d55aee1e7ecab4a8609a50f88e1
Somil Sharma <somil8181@gmail.com>
2026-06-10 22:18:38 +0530

    Create Convert  lower Case to upper case.java

---
+++ Convert  lower Case to upper case.java
@@ -0,0 +1,7 @@
+public class Convertlctouc {
+    public static void main(String[] args) {
+        String str  = "java  programming is a best language";
+
+        System.out.println(str.toUpperCase());
+    }
+}
