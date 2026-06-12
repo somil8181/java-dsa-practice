c402f98ca64955dbaba045056c881a3ac742e55b
Somil Sharma <somil8181@gmail.com>
2026-06-12 09:45:54 +0530

    Create LinkedList in add , remove .java

---
+++ LinkedList in add , remove .java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class LinkedList1 {
+    public static void main(String[]args){
+     LinkedList<Integer> ll= new LinkedList<>();
+
+     ll.add(5);
+     ll.add(6);
+     ll.add(7);
+     ll.add(8);
+System.out.println(ll);
+
+
+ll.remove(3);
+System.out.println(ll); 
+
+
+
+
+ll.set(2,3);
+System.out.println(ll);
+
+
+
+     
+    }
+}
