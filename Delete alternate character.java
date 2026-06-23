a68f4b2e8ff6149da72fc9abf62eeec7a7574c24
Somil Sharma <somil8181@gmail.com>
2026-06-23 23:10:45 +0530

    Create Delete alternate character.java

---
+++ Delete alternate character.java
@@ -0,0 +1,13 @@
+class Solution {
+    static String delAlternate(String s) {
+      StringBuilder  sb = new  StringBuilder();
+      for(int i =0;  i<s.length(); i++);
+      if(i%2==0){
+        sb.append(s.charAt(i));
+              
+           }
+       }
+        return sb.toString();
+        
+    }
+}
